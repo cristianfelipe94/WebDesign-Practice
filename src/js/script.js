4581b7c6f1c10d1e33c6a07ec40b0038d9adb005
Cristian Felipe <cristianpipe0494@gmail.com>
2019-05-17 18:57:11 -0600

    Have removed red color from content boxes on Bio Section.

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,17 @@
 (function IIFE() {
-	const actionOpenBio = document.querySelectorAll('.js-open-bio');
-	const containerOpenClose = document.querySelectorAll('.js-bio-container');
+	const actionOpenBio = document.getElementById('js-open-bio');
+	const containerOpenClose = document.getElementById('js-bio-container');
 
 	let togglerBio = false;
 
-	actionOpenBio.forEach((element) => {
-		element.addEventListener('click', () => {
-			if (togglerBio === false) {
-				containerOpenClose.setAttribute('class', 'bio-content-showing');
-				togglerBio = true;
-			} else {
-				containerOpenClose.setAttribute('class', 'bio-content-hidden');
-				togglerBio = false;
-			}
-		});
+	actionOpenBio.addEventListener('click', () => {
+		if (togglerBio === false) {
+			containerOpenClose.setAttribute('class', 'bio-content-showing');
+			togglerBio = true;
+		} else {
+			containerOpenClose.setAttribute('class', 'bio-content-hidden');
+			togglerBio = false;
+		}
 	});
 
 }());
