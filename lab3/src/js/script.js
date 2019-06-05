0778a97b63f5c61bffa61ba4bbae6827c2b6c155
Cristian Felipe <cristianpipe0494@gmail.com>
2019-06-05 13:48:30 -0600

    Have added functionality to open images from posts.

--- lab3/src/js/script.js
+++ lab3/src/js/script.js
@@ -1,17 +1,41 @@
 (function IIFE() {
 	const actionOpenBio = document.getElementById('js-open-bio');
+
+	const actionOpenImg = document.getElementById('js-img-opener');
+
+	const imageBlur = document.getElementById('js-img-blur');
+	
+
 	const containerOpenClose = document.getElementById('js-bio-container');
 
+	const containerBioOpenClose = document.getElementById('js-bio-container');
+
+	const containerImgOpenClose = document.getElementById('js-img-container');
+	
+
 	let togglerBio = false;
+	let togglerImg = false;
 
 	actionOpenBio.addEventListener('click', () => {
 		if (togglerBio === false) {
-			containerOpenClose.setAttribute('class', 'bio-content-showing');
+			containerBioOpenClose.setAttribute('class', 'bio-content-showing');
 			togglerBio = true;
 		} else {
-			containerOpenClose.setAttribute('class', 'bio-content-hidden');
+			containerBioOpenClose.setAttribute('class', 'bio-content-hidden');
 			togglerBio = false;
 		}
 	});
 
+
+	actionOpenImg.addEventListener('click', () => {
+		if (togglerImg === true) {
+			containerImgOpenClose.setAttribute('class', 'post-container-hidden');
+			imageBlur.setAttribute('class', 'post-img-blur');
+			togglerImg = false;
+		} else {
+			containerImgOpenClose.setAttribute('class', 'post-container-showing');
+			imageBlur.setAttribute('class', 'post-img-Noblur');
+			togglerImg = true;
+		}
+	});
 }());
